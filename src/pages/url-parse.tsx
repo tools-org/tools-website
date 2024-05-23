ea66607f155237cbba6d71963b2c10a4b5fbbd54
ChrisSong <1352411860@qq.com>
2024-05-23 15:25:36 +0800

    chore: 基础框架搭建

--- src/pages/url-parse.tsx
+++
@@ -1,8 +0,0 @@
-import UrlParse from '@/tools/UrlParse';
-import PageWrap from '@/components/PageWrap';
-
-export default PageWrap({
-  children: UrlParse,
-  title: 'URL 解析',
-  description: '解析 url ',
-});
