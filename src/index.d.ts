652d4be558ca6830a624f118c32611f1c688c05f
ChrisSong <1352411860@qq.com>
2024-06-07 15:25:05 +0800

    feat: markdown 编辑器

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,8 @@
 declare module 'jsonlint-mod';
 declare module 'react-color/*';
 declare module 'Base64';
+declare module '*.md';
+declare module '*.txt';
 declare module '*.svg' {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
