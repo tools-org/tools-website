b040a651599a83b07e0051f4b9e5260a4252fb82
ChrisSong <1352411860@qq.com>
2024-06-09 14:59:48 +0800

    fix: 颜色选择器问题修复

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,21 +1,24 @@
 import { isEmpty } from 'lodash';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import { INITAL_STORE } from '@/constants';
 import { TOOLS_KEY_ENUM } from '@/types';
 
 const useStore = (key: TOOLS_KEY_ENUM) => {
   const localStoreValue = JSON.parse(localStorage.getItem(key) || '{}');
-
+  console.log(INITAL_STORE, JSON.stringify(INITAL_STORE, null, 2));
   const [data, setData] = useState(
     isEmpty(localStoreValue) ? INITAL_STORE[key] : localStoreValue,
   );
 
-  const setStoreData = (value: any) => {
-    const newData = { ...data, ...value };
-    setData(newData);
-    localStorage.setItem(key, JSON.stringify(newData));
-  };
+  const setStoreData = useCallback(
+    (value: any) => {
+      const newData = { ...data, ...value };
+      setData(newData);
+      localStorage.setItem(key, JSON.stringify(newData));
+    },
+    [data],
+  );
 
   return { storeData: data, setStoreData };
 };
